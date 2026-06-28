123bb77aec0b5e199574b51a82bd98ab996be460
KshitijSawhney <58725102+KshitijSawhney@users.noreply.github.com>
2026-06-28 04:37:36 -0400

    added configs and inital joint code

--- joint.lua
+++ joint.lua
@@ -2,21 +2,12 @@ local cfg = require("config")
 
 rednet.open(cfg.modem)
 rednet.host("joint", cfg.hostname)
-
 local gear = peripheral.wrap(cfg.gearshift)
 
-
 local function execute(delta)
-
-    if math.abs(delta) < 0.1 then
-        return
-    end
-
-    -- Finish any current motion first
     while gear.isRunning() do
         sleep(0)
     end
-
     gear.setInstructions({
         {
             type = "turn_angle",
@@ -27,16 +18,12 @@ local function execute(delta)
             type = "end"
         }
     })
-
     gear.start()
 
 end
 
-
 while true do
-
     local _, msg = rednet.receive("joint.command")
-
     execute(msg.delta)
 
 end
