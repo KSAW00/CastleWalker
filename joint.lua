92fa65c5a55307e047856b7c442b82785e75b3f3
KshitijSawhney <58725102+KshitijSawhney@users.noreply.github.com>
2026-06-28 06:51:18 -0400

    updated joint logic

--- joint.lua
+++ joint.lua
@@ -2,31 +2,45 @@ local cfg = require("config")
 
 rednet.open(cfg.modem)
 rednet.host("joint", cfg.hostname)
-local gear = peripheral.wrap(cfg.gearshift)
 
-local function execute(delta)
+local gear = peripheral.wrap(cfg.gearshift)
+local currentAngle = cfg.home or 0
+
+local function round(x)
+    if x >= 0 then
+        return math.floor(x + 0.5)
+    else
+        return math.ceil(x - 0.5)
+    end
+end
+
+local function execute(targetAngle)
+    targetAngle = round(targetAngle)
+    if targetAngle == currentAngle then
+        return
+    end
+    local delta = targetAngle - currentAngle
     while gear.isRunning() do
         sleep(0.05)
     end
+
     gear.setInstructions({
         {
             type = "turn_angle",
             value = math.abs(delta),
-            speed_modifier = 1
+            speed_modifier = delta >= 0 and 1 or -1
         },
         {
             type = "end"
         }
     })
-    gear.start()
 
+    gear.start()
+    
+    currentAngle = targetAngle
 end
 
 while true do
     local _, msg = rednet.receive("joint.command")
-    execute(msg.delta)
-
+    execute(msg.angle)
 end
-
-
-
